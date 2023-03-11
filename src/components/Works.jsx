473755a672fb88956b26324acbdd9e5ecd69a6ff
Emmanuel527 <arowoseluemmanuel@gmail.com>
2023-03-11 17:21:59 -0600

    commit

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -40,7 +40,9 @@ return(
       </div>
 
       <div className='mt-4 flex flex-wrap gap-2'>
-        {tags.map((tag) =>(<p key = {tag.name} className=''></p>))}
+        {tags.map((tag) =>(<p key = {tag.name} className={`text-[14px] ${tag.color}`}>
+          #{tag.name}
+        </p>))}
 
       </div>
 
